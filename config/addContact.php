27090df60339310367e29a271c7f9d241be4bfc1
derikincode <derikoliveira252@gmail.com>
2024-05-13 19:04:40 -0300

    UPDATE

--- config/addContact.php
+++ config/addContact.php
@@ -1,28 +1,22 @@
 <?php
-include 'dbConnection.php'; // inclui o arquivo de conexão
-// Verifica se os dados foram enviados via POST
+include 'dbConnection.php'; 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Captura os dados do formulário
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 
-    // Prepara a declaração SQL para inserção de dados
     $sql = "INSERT INTO contact (user, email, phone) VALUES (?, ?, ?)";
 
-    // Prepara a declaração
     if ($stmt = $connection->prepare($sql)) {
-        // Vincula parâmetros à declaração
         $stmt->bind_param("sss", $name, $email, $phone);
 
-        // Executa a declaração
         if ($stmt->execute()) {
             echo "Contato adicionado com sucesso!";
         } else {
             echo "Erro ao adicionar contato: " . $stmt->error;
         }
 
-        // Fecha a declaração
         $stmt->close();
     } else {
         echo "Erro na preparação da declaração: " . $connection->error;
--- config/countContacts.php
+++ config/countContacts.php
@@ -1,7 +1,6 @@
 <?php
 include 'dbConnection.php';
 
-// Verifica se há erro na conexão
 if ($connection->connect_error) {
     die("Erro de conexão: " . $connection->connect_error);
 }
@@ -11,14 +10,11 @@ $sql = "SELECT COUNT(*) AS total FROM contact";
 $resultado = $connection->query($sql);
 
 if ($resultado->num_rows > 0) {
-    // Extrai o resultado da consulta
     $row = $resultado->fetch_assoc();
     $total_contatos = $row['total'];
 } else {
-    // Se nenhum contato for encontrado, define o total como 0
     $total_contatos = 0;
 }
 
-// Fecha a conexão com o banco de dados
 $connection->close();
 ?>
